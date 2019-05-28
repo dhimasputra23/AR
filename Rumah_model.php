f8eb280cfc67255bccd29ca4297c8cba12d297e9
Dhimas Putranto Budhi Harto <dhimasputrabudhi@gmail.com>
2019-05-29 04:57:12 +0700

    CI_Model Rumah

---
+++ Rumah_model.php
@@ -0,0 +1,73 @@
+<?php 
+
+/**
+ * 
+ */
+class Rumah_model extends CI_Model
+{
+	
+	public function getAllRumah()
+	{
+
+        $rumah = $this->db->get('tb_rumah')->result_array();
+
+        return $rumah;
+	}
+
+
+    public function tambahDataRumah()
+    {
+        $data = [
+            "deskripsi" => $this->input->post('deskripsi', true),
+            "nama_file" => $this->input->post('nama_file', true),
+            "tipe_rumah" => $this->input->post('tipe_rumah', true),
+            "luas_tanah" => $this->input->post('luas_tanah', true),
+            "luas_bangunan" => $this->input->post('luas_bangunan', true),
+            "lantai" => $this->input->post('lantai', true),
+            "listrik" => $this->input->post('listrik', true),
+            "tahun" => $this->input->post('tahun', true),
+            "harga" => $this->input->post('harga', true),
+            "pengembang" => $this->input->post('pengembang', true),
+        ];
+
+        $this->db->insert('tb_rumah', $data);
+    }
+
+
+     public function hapusDataRumah($id)
+    {
+        // $this->db->where('id',$id);
+
+        $this->db->delete('tb_rumah', ['id' => $id]);
+    }
+
+        public function getRumahId($id)
+    {
+        $this->db->select('*');
+        $this->db->from('tb_rumah');
+        $this->db->where('id', $id);
+        $rumah = $this->db->get()->row_array();
+        return $rumah;
+    }
+
+
+    public function ubahDataRumah()
+    {
+         $data = [
+            "deskripsi" => $this->input->post('deskripsi', true),
+            "nama_file" => $this->input->post('nama_file', true),
+            "tipe_rumah" => $this->input->post('tipe_rumah', true),
+            "luas_tanah" => $this->input->post('luas_tanah', true),
+            "luas_bangunan" => $this->input->post('luas_bangunan', true),
+            "lantai" => $this->input->post('lantai', true),
+            "listrik" => $this->input->post('listrik', true),
+            "tahun" => $this->input->post('tahun', true),
+            "harga" => $this->input->post('harga', true),
+            "pengembang" => $this->input->post('pengembang', true),
+        ];
+        $this->db->where('id', $this->input->post('id'));
+        $this->db->update('tb_rumah', $data);
+    }
+}
+
+?>
