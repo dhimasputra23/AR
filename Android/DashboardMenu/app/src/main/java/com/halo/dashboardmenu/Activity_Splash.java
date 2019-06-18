a4ad0fa19c3d37944894bd48d165767e41cee56c
MikaelAdi <mikaeladi1997@gmail.com>
2019-06-18 14:17:48 +0700

    Test Splash Screen

---
+++ Android/DashboardMenu/app/src/main/java/com/halo/dashboardmenu/Activity_Splash.java
@@ -0,0 +1,32 @@
+package com.halo.dashboardmenu;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Splash extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 4000; //This is 4 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Code to start timer and take action after the timer ends
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do any action here. Now we are moving to next page
+                Intent mySuperIntent = new Intent(Activity_Splash.this, Activity_Main.class);
+                startActivity(mySuperIntent);
+                /* This 'finish()' is for exiting the app when back button pressed
+                 *  from Home page which is ActivityHome
+                 */
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
+
